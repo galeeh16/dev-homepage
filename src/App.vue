3d503cb7f0e72ee0da0d3bbfb738d668a32aa1c9
Galih Anggoro Jati <anggorogalih8@gmail.com>
2022-12-21 16:33:59 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="bg-blue banner" style="position: relative;">
+    <div class="container">
+      <div class="d-flex justify-content-between">
+        <a href="/" style="text-decoration: none;">
+          <h3 class="d-flex align-items-center" style="gap: 8px; font-size: 24px; color: rgb(10, 32, 112);">
+            <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="">
+              <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd"
+                d="M9.18824 3.74722C9.18824 3.33438 8.84724 3 8.42724 3H8.42624L6.79724 3.00098C4.60624 3.00294 2.82324 4.75331 2.82324 6.90279V8.76201C2.82324 9.17386 3.16424 9.50923 3.58424 9.50923C4.00424 9.50923 4.34624 9.17386 4.34624 8.76201V6.90279C4.34624 5.57604 5.44624 4.4964 6.79824 4.49444L8.42724 4.49345C8.84824 4.49345 9.18824 4.15907 9.18824 3.74722ZM17.1931 3.00029H15.6001C15.1801 3.00029 14.8391 3.33468 14.8391 3.74751C14.8391 4.15936 15.1801 4.49277 15.6001 4.49277H17.1931C18.5501 4.49277 19.6541 5.57535 19.6541 6.90603V8.7623C19.6541 9.17415 19.9951 9.50952 20.4151 9.50952C20.8361 9.50952 21.1761 9.17415 21.1761 8.7623V6.90603C21.1761 4.75165 19.3901 3.00029 17.1931 3.00029ZM9.23804 6.74266H14.762C15.367 6.74266 15.948 6.98094 16.371 7.40554C16.797 7.83407 17.033 8.40968 17.032 9.00883V10.2542C17.027 10.4003 16.908 10.5189 16.759 10.5229H7.23904C7.09104 10.518 6.97204 10.3993 6.96904 10.2542V9.00883C6.95804 7.76837 7.97404 6.75541 9.23804 6.74266Z"
+                fill="currentColor"></path>
+              <path
+                d="M22.239 12.0413H1.762C1.342 12.0413 1 12.3756 1 12.7885C1 13.2003 1.342 13.5337 1.762 13.5337H2.823V17.0972C2.823 19.2467 4.607 20.9971 6.798 20.999L8.427 21C8.848 21 9.188 20.6656 9.189 20.2528C9.189 19.841 8.848 19.5066 8.428 19.5066L6.8 19.5056C5.447 19.5036 4.346 18.424 4.346 17.0972V13.5337H6.969V14.5251C6.959 15.7656 7.974 16.7795 9.238 16.7913H14.762C16.027 16.7795 17.042 15.7656 17.032 14.5251V13.5337H19.655V17.0933C19.655 18.425 18.551 19.5066 17.194 19.5066H15.601C15.18 19.5066 14.839 19.841 14.839 20.2528C14.839 20.6656 15.18 21 15.601 21H17.194C19.39 21 21.177 19.2487 21.177 17.0933V13.5337H22.239C22.659 13.5337 23 13.2003 23 12.7885C23 12.3756 22.659 12.0413 22.239 12.0413Z"
+                fill="currentColor"></path>
+            </svg> Core Team
+          </h3>
+        </a>
+        <div class="dropdown" data-v-b913ba79="">
+          <div class="d-flex align-items-center text-dark" data-v-b913ba79=""
+            style="gap: 5px; justify-content: space-between;">
+            <div data-v-b913ba79=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                data-v-b913ba79="">
+                <path class="st0" d="M2,16c0.1,0,8-5,9-7c0.6-1.3,1-5,1-5h3H1h7V1" data-v-b913ba79=""></path>
+                <line class="st0" x1="4" y1="8" x2="12" y2="16" data-v-b913ba79=""></line>
+                <polygon class="st0" points="15,19 21,19 23,23 18,11 13,23 " data-v-b913ba79=""></polygon>
+              </svg></div>
+            <div class="" data-v-b913ba79="" style="font-weight: 600; font-size: 18px; text-transform: uppercase;">en
+            </div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+              class="feather feather-chevron-down" data-v-b913ba79="">
+              <polyline points="6 9 12 15 18 9" data-v-b913ba79=""></polyline>
+            </svg>
+          </div>
+          <div class="dropdown-content" data-v-b913ba79="" style="">
+            <div class="d-flex align-items-center" data-v-b913ba79="" style="gap: 8px;"><svg
+                xmlns="http://www.w3.org/2000/svg" width="18" height="18" id="flag-icons-id" viewBox="0 0 640 480"
+                data-v-b913ba79="">
+                <path fill="#e70011" d="M0 0h640v240H0Z" data-v-b913ba79=""></path>
+                <path fill="#f4f5f6" d="M0 240h640v240H0Z" data-v-b913ba79=""></path>
+              </svg> ID </div><!---->
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between">
+        <div style="margin-top: 130px; max-width: 410px;">
+          <h1 class="rainbow-text" style="font-size: 64px;">Core Team</h1>
+          <h2 style="font-weight: 500; font-size: 34px;">Ultrastack Developer</h2>
+          <p class="text-dark" style="font-size: 17px;">We build, we serve, we deploy and maintenance everything.</p><a
+            href="#teams" class="btn d-flex align-items-center"
+            style="margin-top: 1.5rem; gap: 5px; position: relative; z-index: 4;">Get Started <svg
+              xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+              class="feather feather-arrow-right">
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+              <polyline points="12 5 19 12 12 19"></polyline>
+            </svg></a>
+        </div>
+        <div class="animate-banner" style="max-width: 720px; position: relative; z-index: 2;"><img class="img-banner"
+            :src="GlobeImage" alt="illustration" style="width: 100%; object-fit: cover;"></div>
+      </div>
+    </div>
+
+    <div class="svg-white" style="position: absolute; bottom: -80px; left: 0px; right: 0px;"><svg
+        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#f3f4f6" fill-opacity="1"
+          d="M0,192L40,192C80,192,160,192,240,176C320,160,400,128,480,106.7C560,85,640,75,720,106.7C800,139,880,213,960,229.3C1040,245,1120,203,1200,160C1280,117,1360,75,1400,53.3L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z">
+        </path>
+      </svg></div>
+  </div>
+
+  <Team />
+</template>
+
+<script setup>
+import GlobeImage from './assets/globe.png';
+
+import Team from './components/Team.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Team.vue
@@ -0,0 +1,28 @@
+<template>
+	<section id="teams" sytle="margin-top: 30px; margin-bottom: 100px; padding: 50px 0px; position: relative;">
+		<div class="d-flex justify-content-center">
+			<h2 class="text-center h2-text">Meet Our Team</h2>
+		</div>
+		<p class="text-center" style="margin-bottom: 40px;">Meet our team member who create CBS Web and many more apps</p>
+
+		<div class="container">
+			<div class="grid">
+				<div class="team" v-for="i in 7" :key="i">
+					<div class="team-image">
+						<img :src="HackerImage" alt="Kurniawan Halim" style="width: 100%; max-width: 170px;">
+					</div>
+					<div class="team-body">
+						<p class="text-dark" style="font-size: 18px; font-weight: 500;">Kurniawan Halim</p>
+						<p class="text-gray" style="font-size: 15px;">Lead of Core Team</p>
+						<p class="text-gray" style="font-size: 13px; margin-top: 1rem;">18 years experience</p>
+						<p class="text-gray" style="font-size: 13px;">Since 2004-01-01</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup>
+import HackerImage from '../assets/hacker.png';
+</script>
