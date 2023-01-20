c7ecf1ca8f3c0c56816e631e0315d9470887f415
Galih Anggoro Jati <anggorogalih8@gmail.com>
2023-01-20 10:57:22 +0700

    update our project apps

--- src/components/OurProject.vue
+++ src/components/OurProject.vue
@@ -36,8 +36,8 @@
             <td>{{ application.type }}</td>
             <td>
               <a :href="application.url" target="_blank" :title="`${$t('go_to')} ${application.name}`">{{
-    $t('open_app')
-                }} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                $t('open_app')
+              }} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                   class="bi bi-link-45deg" viewBox="0 0 16 16">
                   <path
                     d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z">
@@ -257,6 +257,30 @@ const applications = ref([
     name: 'Auth Efilling',
     type: 'BE',
     url: 'http://10.30.8.50:6140/'
+  },
+  {
+    id: 34,
+    name: 'FE HADIAH KOPNUS',
+    type: 'FE',
+    url: 'http://10.30.8.40:3100/'
+  },
+  {
+    id: 35,
+    name: 'BE HADIAH KOPNUS',
+    type: 'BE',
+    url: 'http://10.30.8.40:6240/'
+  },
+  {
+    id: 36,
+    name: 'API CBS Funding',
+    type: 'BE',
+    url: 'http://10.30.8.40:6220/'
+  },
+  {
+    id: 37,
+    name: 'API CBS Tabungan',
+    type: 'BE',
+    url: 'http://10.30.8.40:6230/'
   },
 ]);
 
